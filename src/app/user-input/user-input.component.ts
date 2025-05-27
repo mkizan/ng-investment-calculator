1eb410d6f2e6a6c8b0a53215490689d1401a5994
Mykola Kizan <mkizan@gmail.com>
2025-05-27 21:47:28 +0300

    create user form

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import type { InvestmentInput } from '../investment-input.model';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css',
+})
+export class UserInputComponent {
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+
+  onSubmit() {
+    console.log('Submitted!');
+    this.calculate.emit({
+      initialInvestment: Number(this.enteredInitialInvestment),
+      duration: Number(this.enteredDuration),
+      expectedReturn: Number(this.enteredExpectedReturn),
+      annualInvestment: Number(this.enteredAnnualInvestment),
+    });
+  }
+}
